1d0450e34433478a150d4972dcbeb9b131f58d99
evanchooly <jlee@antwerkz.com>
2024-02-27 18:04:19 -0500

    specify version

--- .github/test-upgrade.sh
+++ .github/test-upgrade.sh
@@ -8,13 +8,15 @@ mvn -q install -DskipTests
 
 echo updating projecting via openrewrite
 
+MVN_OPTS="-Dmorphia.version=3.0.0-SNAPSHOT"
+
 mvn ${MVN_OPTS} -U org.openrewrite.maven:rewrite-maven-plugin:run \
   -Drewrite.recipeArtifactCoordinates=dev.morphia.morphia:rewrite:3.0.0-SNAPSHOT \
   -Drewrite.activeRecipes=dev.morphia.UpgradeToMorphia30 2>&1 \
   -Drewrite.exclusions="**/*.json" \
   2>&1 | tee rewrite.out
 
-mvn test -Dmorphia.version=3.0.0-SNAPSHOT ${MVN_OPTS}
+mvn test  ${MVN_OPTS}
 
 git ls-files -m | grep "src/test"
 
