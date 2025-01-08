785b00175f55c303d79d3172c4492bc95e249dfa
evanchooly <jlee@antwerkz.com>
2025-01-07 21:09:49 -0500

    use rewrite.sh in the action definition

--- rewrite.sh
+++ rewrite.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-rm -vfr ~/.m2/repository/dev/morphia/morphia/rewrite/
 mvn -e -U \
      org.openrewrite.maven:rewrite-maven-plugin:run \
 		 -Drewrite.recipeArtifactCoordinates=dev.morphia.morphia:rewrite:3.0.0-SNAPSHOT \
